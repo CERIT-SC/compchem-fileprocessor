e879f6f9a842b7978732c87454e2a3e224764d3b
xkollar3 <505734@mail.muni.cz>
2025-09-27 13:02:54 +0200

    feat: adapt templates for authentication

--- argo/read-files/read-files.sh
+++ argo/read-files/read-files.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 <url> <file_id1> [file_id2] [...]"
+  echo "Usage: $0 <url> <secret-key> <file_id1> [file_id2] [...]"
   exit 1
 fi
 
 URL="$1" # https://localhost:5000/api/experiments
 shift
+SECRET="$1"
+shift
 RECORD_ID="$1" # ew6jd-p8175
-shift 
+shift
 
 DOWNLOAD_DIR="/output"
 mkdir -p $DOWNLOAD_DIR
 
 for FILE_ID in "$@"; do
-  DOWNLOAD_URL="${URL}/${RECORD_ID}/draft/files/${FILE_ID}/content"
+  DOWNLOAD_URL="${URL}/${RECORD_ID}/draft/files/${FILE_ID}/workflow-content?secret_key=${SECRET}"
   OUTPUT_FILE="${DOWNLOAD_DIR}/${FILE_ID}"
   echo "Downloading from: $DOWNLOAD_URL"
   echo "Saving to: $OUTPUT_FILE"
-  
+
   REDIRECT_OUTPUT=$(wget --no-check-certificate --header="Host: localhost:5000" --max-redirect=0 "$DOWNLOAD_URL" -O "$OUTPUT_FILE" 2>&1)
   REDIRECT_URL=$(echo "$REDIRECT_OUTPUT" | grep -o "Location:.*" | cut -d' ' -f2- | sed 's/ \[following\]$//' | sed 's/127.0.0.1/172.22.0.3/')
-  
+
   if [ -z "$REDIRECT_URL" ]; then
     echo "Failed to get redirect URL from ${DOWNLOAD_URL}"
     echo "$REDIRECT_OUTPUT"
     exit 1
   fi
-  
+
   echo "Redirected to: $REDIRECT_URL"
-  
-  wget --header="Host: 127.0.0.1:9000" -O "$OUTPUT_FILE" "$REDIRECT_URL" || { 
-    echo "Failed to download from redirect URL"; 
-    exit 1; 
+
+  wget --header="Host: 127.0.0.1:9000" -O "$OUTPUT_FILE" "$REDIRECT_URL" || {
+    echo "Failed to download from redirect URL";
+    exit 1;
   }
 done
-
--- argo/write-files/write-files.sh
+++ argo/write-files/write-files.sh
@@ -6,10 +6,11 @@ BASE_URL="$1"
 RECORD_ID="$2"
 WORKFLOW_NAME="$3"
 TASK_DISCRIMINATOR="$4"
+SECRET_KEY="$5"
 FILES_DIR="/input"
 
 if [ -z "$BASE_URL" ] || [ -z "$RECORD_ID" ]; then
-  echo "Usage: $0 <base_url> <record_id> <workflow_name> <task_discriminator>"
+  echo "Usage: $0 <base_url> <record_id> <workflow_name> <task_discriminator> <secret_key>"
   exit 1
 fi
 
@@ -22,25 +23,10 @@ for FILE_PATH in "$FILES_DIR"/*; do
   FILE_NAME=$WORKFLOW_NAME-$TASK_DISCRIMINATOR-$(basename "$FILE_PATH")
   echo "Uploading file: $FILE_NAME"
 
-  # TODO: temporary curl headers, -k -H
-  # TODO: what is the purpose here? do we delete the old file? or keep creating new ones?
-  echo "Registering metadata"
-  curl -f -k -H "Host: localhost" -X POST "${BASE_URL}/${RECORD_ID}/draft/files" \
-    -H "Content-Type: application/json" \
-    -d "[{\"key\": \"${FILE_NAME}\"}]" || { echo "Failed to register $FILE_NAME"; exit 1; }
-
-  echo "\n"
-
   echo "Uploading content"
-    curl -f -k -H "Host: localhost" -H "Content-Type: application/octet-stream" -X PUT "${BASE_URL}/${RECORD_ID}/draft/files/${FILE_NAME}/content" \
+    curl -f -k -H "Host: localhost" -H "Content-Type: application/octet-stream" -X PUT "${BASE_URL}/${RECORD_ID}/draft/files/${FILE_NAME}/workflow-commit?secret_key=${SECRET_KEY}" \
     --data-binary "@${FILE_PATH}" || { echo "Failed to upload content for $FILE_NAME"; exit 1; }
 
-  echo "\n"
-
-  echo "Committing file"
-  curl -f -k -H "Host: localhost" -X POST "${BASE_URL}/${RECORD_ID}/draft/files/${FILE_NAME}/commit" || { echo "Failed to commit $FILE_NAME"; exit 1; }
-
-  echo "$FILE_NAME uploaded and committed successfully"
 done
 
 echo "All files uploaded successfully."
